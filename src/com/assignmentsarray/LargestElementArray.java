7b886d044bf78260889603b8a2d590ee4a9f2c1c
AdeebaSultana <adizaan8892@gmail.com>
2022-04-01 22:28:04 +0530

    Added OddPositionArray.java Program

--- src/com/assignmentsarray/LargestElementArray.java
+++ src/com/assignmentsarray/LargestElementArray.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class LargestElementArray {
+package com.assignmentsarray;
+
+public class LargestElementArray {
+    public static void main(String[] args) {
+        int[] arr={10,60,30,40,90,50,100,70,12};
+        int max=arr[0];
+        for (int j : arr) {
+            if (j > max) {
+                max = j;
+            }
+        }
+        System.out.println("The largest number in the given array is : ");
+        System.out.println(max);
+    }
 }
